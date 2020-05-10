3c4f1686438b5a42f72e01eee289c8f8c6dc4b4e
Arjun ghimire <developerarjun1@gmail.com>
2020-05-10 21:59:25 +0545

    multiple component

--- src/App.vue
+++ src/App.vue
@@ -2,25 +2,43 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12">
-                <app-user></app-user>
+                <button @click="selectedComponent = 'appQuote'">Quote</button>
+                <button @click="selectedComponent = 'appAuthor'">Author</button>
+                <button @click="selectedComponent = 'appNew'">New</button>
+                <hr>
+                <p>{{ selectedComponent }}</p>
+                <!-- <app-quote>
+                    <h2 slot="title">{{ quoteTitle }}</h2>
+                    <p>A Wonderful Quote</p>
+                </app-quote> !-->
+                <keep-alive>
+                    <component :is="selectedComponent">
+                        <p>Defaul slot</p>
+                    </component>
+                </keep-alive>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-    import User from './components/User.vue';
-
+    import Quote from './components/Quote';
+    import Author from './components/Author';
+    import New from './components/New';
     export default {
-        components: {
-            appUser: User
-        }
+        data(){
+            return{
+                quoteTitle : 'The Quote',
+                selectedComponent: 'appQuote',
+            }
+        },
+       components:{
+           appQuote: Quote,
+           appAuthor: Author,
+           appNew: New
+       } 
     }
 </script>
 
 <style>
-    div.component {
-        border: 1px solid black;
-        padding: 30px;
-    }
 </style>
---
+++ src/components/New.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h3>
+            New Quote
+            <button @click="counter++">Increase</button>
+            <p>{{ counter }}</p>
+        </h3>
+    </div>   
+</template>
+<script>
+export default {
+    data: function(){
+        return{
+            counter: 0
+        };
+    },
+    destroyed: function(){
+        console.log("Destroyed !!");
+    },
+    deactivated(){
+        console.log("Deactived !!");
+    },
+    activated(){
+        console.log("Activated !!");
+    }
+}
+</script>
+<style>
+</style>
